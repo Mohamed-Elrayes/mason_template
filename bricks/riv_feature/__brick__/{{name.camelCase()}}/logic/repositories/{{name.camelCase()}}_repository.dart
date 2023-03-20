f4c44cfca418369ecf250727e89d59f59337213d
Mohamed Salah <52123133+Mohamed-Elrayes@users.noreply.github.com>
2023-03-20 17:45:03 +0200

    N

--- bricks/riv_feature/__brick__/{{name.camelCase()}}/logic/repositories/{{name.camelCase()}}_repository.dart
+++ bricks/riv_feature/__brick__/{{name.camelCase()}}/logic/repositories/{{name.camelCase()}}_repository.dart
@@ -3,7 +3,8 @@ import '../models/{{name.camelCase()}}_model.dart';
 
 import 'http_{{name.camelCase()}}_repository.dart';
 
-
+/// Provider to map [Http{{name.pascalCase()}}Repository] implementation to
+/// [{{name.pascalCase()}}Repository] interface
 final {{name.camelCase()}}RepositoryProvider = Provider<{{name.pascalCase()}}Repository>(
   (ref) {
     final httpService = ref.watch(httpServiceProvider);
@@ -11,10 +12,10 @@ final {{name.camelCase()}}RepositoryProvider = Provider<{{name.pascalCase()}}Rep
   },
 );
 
-/// Home repository interface
+/// {{name.pascalCase()}} repository interface
 abstract class {{name.pascalCase()}}Repository {
   // implement endpoint
   String get endpoint;
-
+  /// Request to get a {{name.pascalCase()}}  endpoint
   Future<{{name.pascalCase()}}Model> get{{name.pascalCase()}}();
 }
